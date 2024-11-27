d3eca543b81a9fbeda649c6e0f0b755f2ae37d5d
y0014984 <y0014984@gmail.com>
2024-11-27 19:11:09 +0100

    fixed draw letter

--- src/logic/Graphics.ts
+++ src/logic/Graphics.ts
@@ -22,8 +22,8 @@ export default class Graphics {
     checkMemWrite(index: number) {
         if (index >= 256 * 4 && index <= 256 * 7) {
             const tmpIndex = index - 256 * 4;
-            const x = tmpIndex % 32;
-            const y = Math.floor(tmpIndex / 32);
+            const x = tmpIndex % (this.width / 8);
+            const y = Math.floor(tmpIndex / (this.width / 8));
             console.log(`Index: ${index} X: ${x} Y: ${y} Letter(Hex): ${this.mem.getAsHexString(index)}`);
             this.drawLetter(x, y, this.mem.getAsHexString(index));
         }
