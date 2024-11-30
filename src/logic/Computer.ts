72734ed19ab798f6e8d0f132632612cac0fc8f72
y0014984 <y0014984@gmail.com>
2024-11-30 19:35:13 +0100

    added more keypress register bits

--- src/logic/Computer.ts
+++ src/logic/Computer.ts
@@ -64,6 +64,134 @@ export default class Computer {
                 address = '0200';
                 bitIndex = 7;
                 break;
+            case 'KeyI':
+                address = '0201';
+                bitIndex = 0;
+                break;
+            case 'KeyJ':
+                address = '0201';
+                bitIndex = 1;
+                break;
+            case 'KeyK':
+                address = '0201';
+                bitIndex = 2;
+                break;
+            case 'KeyL':
+                address = '0201';
+                bitIndex = 3;
+                break;
+            case 'KeyM':
+                address = '0201';
+                bitIndex = 4;
+                break;
+            case 'KeyN':
+                address = '0201';
+                bitIndex = 5;
+                break;
+            case 'KeyO':
+                address = '0201';
+                bitIndex = 6;
+                break;
+            case 'KeyP':
+                address = '0201';
+                bitIndex = 7;
+                break;
+            case 'KeyQ':
+                address = '0202';
+                bitIndex = 0;
+                break;
+            case 'KeyR':
+                address = '0202';
+                bitIndex = 1;
+                break;
+            case 'KeyS':
+                address = '0202';
+                bitIndex = 2;
+                break;
+            case 'KeyT':
+                address = '0202';
+                bitIndex = 3;
+                break;
+            case 'KeyU':
+                address = '0202';
+                bitIndex = 4;
+                break;
+            case 'KeyV':
+                address = '0202';
+                bitIndex = 5;
+                break;
+            case 'KeyW':
+                address = '0202';
+                bitIndex = 6;
+                break;
+            case 'KeyX':
+                address = '0202';
+                bitIndex = 7;
+                break;
+            case 'KeyY':
+                address = '0203';
+                bitIndex = 0;
+                break;
+            case 'KeyZ':
+                address = '0203';
+                bitIndex = 1;
+                break;
+            case 'Digit1':
+                address = '0203';
+                bitIndex = 2;
+                break;
+            case 'Digit2':
+                address = '0203';
+                bitIndex = 3;
+                break;
+            case 'Digit3':
+                address = '0203';
+                bitIndex = 4;
+                break;
+            case 'Digit4':
+                address = '0203';
+                bitIndex = 5;
+                break;
+            case 'Digit5':
+                address = '0203';
+                bitIndex = 6;
+                break;
+            case 'Digit6':
+                address = '0203';
+                bitIndex = 7;
+                break;
+            case 'Digit7':
+                address = '0204';
+                bitIndex = 0;
+                break;
+            case 'Digit8':
+                address = '0204';
+                bitIndex = 1;
+                break;
+            case 'Digit9':
+                address = '0204';
+                bitIndex = 2;
+                break;
+            case 'Digit0':
+                address = '0204';
+                bitIndex = 3;
+                break;
+            case 'Minus':
+                address = '0204';
+                bitIndex = 4;
+                break;
+            case 'Equal':
+                address = '0204';
+                bitIndex = 5;
+                break;
+            case 'Comma':
+                address = '0204';
+                bitIndex = 6;
+                break;
+            case 'Period':
+                address = '0204';
+                bitIndex = 7;
+                break;
             default: {
                 break;
             }
